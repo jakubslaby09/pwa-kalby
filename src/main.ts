49d1aa0de273ec33d9a2a5a8baca92c47dc82a68
jakubslaby09 <jakubslaby09@gmail.com>
2022-06-29 18:36:03 +0200

    odebrat modul service workeru

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
 import './modules/elements'
 import './modules/router'
-import './modules/offline'
+//import './modules/offline'
 import * as sounds from './modules/sound'
 
 import './modules/calendar'
